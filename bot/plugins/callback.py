3a9cc8d005f82e3d66b22d23c1a41390bc2a9fd8
eo792 <85933934+eo792@users.noreply.github.com>
2021-06-24 13:40:50 +0530

    Update callback.py

--- bot/plugins/callback.py
+++ bot/plugins/callback.py
@@ -95,7 +95,7 @@ async def cb_navg(bot, update: CallbackQuery):
 
     if not int(index_val) == 0:    
         temp_results.append([
-            InlineKeyboardButton(f"🔰 Page {index_val + 1}/{len(results) if len(results) < max_pages else max_pages} 🔰", callback_data="ignore")
+            InlineKeyboardButton(f"📚 Page {index_val + 1}/{len(results) if len(results) < max_pages else max_pages} 📚", callback_data="ignore")
         ])
     
     if show_invite and int(index_val) !=0 :
